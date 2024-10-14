79bc23f3ea6163a540440e76b5f415036db0c299
VarshiniGowda.V <varshinigowdav14@gmail.com>
2024-10-14 12:52:05 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main() {
+    printf("Hello, World!")
+    printf("%d\n", x);
+    int num = 5;
+    printf("%f\n", num);
+    int arr[5] = {1, 2, 3; 4, 5};
+printf("%d\n", arr[5]);
+printMessage;
+int *ptr = NULL;
+printf("%d\n", *ptr);
+return;
+int unusedVar;
+}
